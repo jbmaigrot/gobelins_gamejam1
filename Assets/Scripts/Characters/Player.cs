ed91c1c4b23976c05d89ed3d63186faf0e304148
jbmaigrot <jb.maigrot@neuf.fr>
2018-10-16 17:05:53 +0200

    First commit

---
+++ Assets/Scripts/Characters/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    private static Player instance;
+
+    private Vector3 startPosition;
+
+    //Other movements variables
+    public Rigidbody2D MyRigidbody { get; set; }
+
+    public static Player Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<Player>();
+            }
+            return instance;
+        }
+    }
+
+    // Use this for initialization
+    public override void Start()
+    {
+        base.Start();
+        startPosition = transform.position;
+        MyRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Flip(horizontal);
+        HandleMovement(horizontal);
+    }
+
+    //Handles running, sliding and jumping of the player
+    private void HandleMovement(float horizontal)
+    {
+        MyRigidbody.velocity = new Vector2(horizontal * movementSpeed, MyRigidbody.velocity.y);
+    }
+
+    //Makes the player turn the other way
+    private void Flip(float horizontal)
+    {
+        //If the player is not sliding or jumping, the player faces the other direction
+        if ((horizontal > 0 && !isFacingRight || horizontal < 0 && isFacingRight))
+        {
+            ChangeDirection();
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager Gm = null;
+
+    void Awake()
+    {
+        if (Gm == null)
+        {
+            Gm = this;
+        }
+        else if (Gm != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Command to restart the level
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Time.timeScale = 1;
+        }
+        //Command Exit
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Exit the game when it is the hub
+            if(SceneManager.GetActiveScene().name=="Hub")
+            {
+                Application.Quit();
+            }
+            //Exit the current level
+            else
+            {
+                SceneManager.LoadScene("Hub");
+            }
+        }
+    }
+    //Load the level
+    public void SetLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        
+    }
+    // Enable the trigger to end the level
+    public void UnlockEnd()
+    {
+        Component[] colliders = Gm.GetComponentsInChildren<Collider2D>(false);
+            foreach(Collider2D collider in colliders)
+        {
+            collider.enabled = true;
+        }
+    }
+}
