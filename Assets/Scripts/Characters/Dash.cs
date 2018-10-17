dbc6d9f1d75dbb4ef8ef68b107f5fcaf9e4c6924
Getlan <donnie.darko2121@gmail.com>
2018-10-17 10:37:13 +0200

    Player changes

--- Assets/Scripts/Characters/Dash.cs
+++ Assets/Scripts/Characters/Dash.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dash : MonoBehaviour {
+public class Dash {
     
     private Vector2 direction;
     private float dashCurrentTime;
