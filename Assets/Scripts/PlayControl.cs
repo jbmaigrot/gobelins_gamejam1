c50659d6307f1cceb9ea9c960dd3915ee1356e7f
Jeremykaito <jh152891@edu.ece.fr>
2018-10-17 16:10:49 +0200

    Player controller mapping

---
+++ Assets/Scripts/PlayControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayControl : MonoBehaviour {
+    private Rigidbody2D rb;
+    public string name;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis(name + "Horizontal");
+        rb.velocity = new Vector2(horizontal * 10, rb.velocity.y);
+
+        if(Input.GetButtonDown(name+"Jump"))
+        {
+            Debug.Log("Jump");
+        }
+
+        if (Input.GetButtonDown(name + "Dash"))
+        {
+            Debug.Log("Dash");
+
+        }
+
+        if (Input.GetAxis(name + "LT")>0 || Input.GetAxis(name + "RT")>0)
+        {
+            Debug.Log("BW");
+
+        }
+
+    }
+}
