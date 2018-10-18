1e87bd05dd4612a89b44f34fbc67805998415dc3
Jeremykaito <jh152891@edu.ece.fr>
2018-10-18 04:42:20 +0200

    c

--- Assets/Scripts/Characters/PlayerBis.cs
+++ Assets/Scripts/Characters/PlayerBis.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerBis : MonoBehaviour
 {
     public Animator MyAnimator { get; private set; }
     [SerializeField]
@@ -16,17 +16,14 @@ public class Player : MonoBehaviour
     // Dash variables
     private Dash dashAction;
     private MovePlayer moveAction;
-    private Jump jumpAction;
     [SerializeField]
     private float dashForce;
     [SerializeField]
     private float dashDuration;
 
     //Jump variables
-    public float maxJumpTime = 0.5f;
-    public float jumpSpeed = 61;
+
     public Transform groundCheck;
-    public AnimationCurve jumpCurve = AnimationCurve.Linear(0, 0, 1, 0);
 
     //Inputs
     float horizontal;
@@ -46,16 +43,6 @@ public class Player : MonoBehaviour
             DashForce = dashForce
         };
 
-        // Jump initialisation
-        jumpAction = new Jump
-        {
-            MaxJumpTime = maxJumpTime,
-            JumpSpeed = jumpSpeed,
-            GroundCheck = groundCheck,
-            JumpCurve = jumpCurve,
-            Player = transform
-        };
-
         // Move initialisation
         moveAction = new MovePlayer();
         moveAction.Initialize(MyAnimator);
@@ -64,13 +51,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-    
+
     }
 
     private void FixedUpdate()
     {
         GetInputs();
-        HandleLayers();
 
         v = rb.velocity;
         Flip(horizontal);
@@ -80,29 +66,18 @@ public class Player : MonoBehaviour
         {
             v = dashAction.Dashing(v);
         }
-        else if(dashButton)
+        else if (dashButton)
         {
             dashAction.StartDash(horizontal, 0);
         }
-        else if(jumpButton)
+        else if (jumpButton)
         {
-            v = jumpAction.QuickJump(v);
+ 
         }
-        else if (jumpButton && jumpAction.IsJumping())
-        {
-            v = jumpAction.LongJump(v);
-
-        }
-        else if (rb.velocity.y < 0)
-        {
-             MyAnimator.SetBool("Land", true);
-            jumpAction.Land();
-        }
-        else 
+        else
         {
             v = moveAction.HandleMovement(horizontal, v);
         }
-
         rb.velocity = v;
     }
 
@@ -130,19 +105,4 @@ public class Player : MonoBehaviour
         rb.transform.localScale = new Vector3(rb.transform.localScale.x * -1, rb.transform.localScale.y, rb.transform.localScale.z);
     }
 
-    //Changes the weight of animator layers
-    private void HandleLayers()
-    {
-        //If the player is in the air, AirLayer is the main layer
-        if (!jumpAction.IsGrounded())
-        {
-            MyAnimator.SetLayerWeight(1, 1);
-        }
-
-        //If the player is on the ground, the ground layer is the main layer
-        else
-        {
-            MyAnimator.SetLayerWeight(1, 0);
-        }
-    }
 }
