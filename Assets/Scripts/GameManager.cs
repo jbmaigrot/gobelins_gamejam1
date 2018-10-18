c1d565f1e8785b87f31f7efad25ef824cf01c05e
Jeremykaito <jh152891@edu.ece.fr>
2018-10-19 00:29:35 +0200

    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager Gm = null;
+
+    void Awake()
+    {
+        if (Gm == null)
+        {
+            Gm = this;
+        }
+        else if (Gm != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Command to restart the level
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Time.timeScale = 1;
+        }
+        //Command Exit
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Exit the game when it is the hub
+            if (SceneManager.GetActiveScene().name == "Menu")
+            {
+                Application.Quit();
+            }
+            //Exit the current level
+            else
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
+    }
+    //Load the level
+    public void SetLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+
+    }
+}
