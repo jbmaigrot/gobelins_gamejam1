38f73bd8cf08e4b9a6fb3144238a6027d0762854
Jeremykaito <jeremykaito@hotmail.fr>
2018-10-25 15:01:49 +0200

    Menu onplayer et credits

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,22 +6,15 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour {
 
     public Button play;
-    public Button quit;
 	// Use this for initialization
 	void Start () {
         AudioManager.instance.Play("MainTheme");
+        play.Select();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        // Command to restart the level
-        if (Input.GetAxis("Vertical")<0)
-        {
-           quit.Select();
-        }
-        else if(Input.GetAxis("Vertical") > 0){
-            play.Select();
-        }
+
 
     }
 
